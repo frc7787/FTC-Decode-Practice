da0f129e777d1184fc23dbdf1bb9aebf1134316b
Sensathoren <koskidennis@outlook.com>
2026-04-16 16:23:52 -0700

    This is a sample first opmode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOp1.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name="Test Op 1")
+public class testOp1 extends OpMode {
+
+    @Override
+    public void init() {}
+
+    @Override
+    public void loop() {}
+}
